c8f211e8c63cdb3a84de24165eed56fef9539318
ChinmayKamath <98154359+ChinmayKamath@users.noreply.github.com>
2022-10-27 21:47:51 +0530

    Update rockpaperscissortest.py

--- rockpaperscissortest.py
+++ rockpaperscissortest.py
@@ -1,12 +1,6 @@
-    # import random module
+    
 import random
-
-# Print multiline instruction
-# performstring concatenation of string
-print("Winning Rules of the Rock paper scissor game as follows: \n"
-								+"Rock vs paper->paper wins \n"
-								+ "Rock vs scissor->Rock wins \n"
-								+"paper vs scissor->scissor wins \n")
+print("WELCOME :)"
 
 while True:
 	print("Enter choice \n 1 for Rock, \n 2 for paper, and \n 3 for scissor \n")
@@ -14,9 +8,10 @@ while True:
 	# take the input from user
 	choice = int(input("User turn: "))
 
-	# OR is the short-circuit operator
-	# if any one of the condition is true
+	'''
+	if any one of the condition is true
 	# then it return True value
+	'''
 	
 	# looping until user enter invalid input
 	while choice > 3 or choice < 1:
@@ -24,7 +19,6 @@ while True:
 		
 
 	# initialize value of choice_name variable
-	# corresponding to the choice value
 	if choice == 1:
 		choice_name = 'Rock'
 	elif choice == 2:
@@ -32,17 +26,11 @@ while True:
 	else:
 		choice_name = 'scissor'
 		
-	# print user choice
 	print("user choice is: " + choice_name)
 	print("\nNow its computer turn.......")
 
-	# Computer chooses randomly any number
-	# among 1 , 2 and 3. Using randint method
-	# of random module
 	comp_choice = random.randint(1, 3)
-	
-	# looping until comp_choice value
-	# is equal to the choice value
+
 	while comp_choice == choice:
 		comp_choice = random.randint(1, 3)
 
@@ -88,4 +76,4 @@ while True:
 		break
 	
 
-print("\nThanks for playing")
+print("\nEND OF GAME :)")
